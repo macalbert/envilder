b6d40ca34b554a3d4df623ff6755a199d84d05c0
Marçal Albert <marcal.albert@m47labs.com>
2024-09-24 22:23:56 +0200

    test: Update test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,35 +1,35 @@
-import { describe, it, expect, vi } from 'vitest';
-import * as fs from 'fs/promises';
-import { SSM } from 'aws-sdk';
-import { run } from '../src/index';
+import { describe, it, expect, vi } from "vitest";
+import * as fs from "fs/promises";
+import { run } from "../src/index";
 
-vi.mock('fs/promises');
-vi.mock('aws-sdk', () => ({
+vi.mock("fs/promises");
+vi.mock("aws-sdk", () => ({
   SSM: vi.fn(() => ({
     getParameter: vi.fn(({ Name }) => ({
-      promise: vi.fn(() => Promise.resolve({ Parameter: { Value: 'mock_value' } })),
+      promise: vi.fn(() =>
+        Promise.resolve({ Parameter: { Value: "mock_value" } })
+      ),
     })),
   })),
 }));
 
-describe('Envilder CLI', () => {
-  it('Should_WriteToEnvFile_When_ValidSSMParamsAreProvided', async () => {
-    
+describe("Envilder CLI", () => {
+  it("Should_generateEnvFileFromSsm_When_", async () => {
     // Arrange
     const mockMap = {
-      "NEXT_PUBLIC_CREDENTIAL_EMAIL": "/path/to/ssm/email"
+      NEXT_PUBLIC_CREDENTIAL_EMAIL: "/path/to/ssm/email",
     };
-    
-    vi.spyOn(fs, 'readFile').mockResolvedValueOnce(JSON.stringify(mockMap));
-    vi.spyOn(fs, 'writeFile').mockResolvedValueOnce();
+
+    vi.spyOn(fs, "readFile").mockResolvedValueOnce(JSON.stringify(mockMap));
+    vi.spyOn(fs, "writeFile").mockResolvedValueOnce();
 
     // Act
-    await run('param_map.json', '.env');
+    await run("param_map.json", ".env");
 
     // Assert
     expect(fs.writeFile).toHaveBeenCalledWith(
       expect.any(String),
-      'NEXT_PUBLIC_CREDENTIAL_EMAIL=mock_value\n'
+      "NEXT_PUBLIC_CREDENTIAL_EMAIL=mock_value"
     );
   });
 });
