79ea1b0903d48205d1cf1b7af46ac5c2f21465da
Marçal Albert <marcal.albert@m47labs.com>
2024-10-01 00:52:16 +0200

    fix: call cliRunner

--- src/cli/cliRunner.ts
+++ src/cli/cliRunner.ts
@@ -1,24 +1,35 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-import { run } from '../index.js';
+import { Command } from "commander";
+import { run } from "../index.js";
 
 export async function cliRunner() {
   const program = new Command();
 
   program
-    .name('envilder')
-    .description('A CLI tool to generate .env files from AWS SSM parameters')
-    .version('0.1.0')
-    .requiredOption('--map <path>', 'Path to the JSON file with environment variable mapping')
-    .requiredOption('--envfile <path>', 'Path to the .env file to be generated');
+    .name("envilder")
+    .description("A CLI tool to generate .env files from AWS SSM parameters")
+    .version("0.1.0")
+    .requiredOption(
+      "--map <path>",
+      "Path to the JSON file with environment variable mapping"
+    )
+    .requiredOption(
+      "--envfile <path>",
+      "Path to the .env file to be generated"
+    );
 
   await program.parseAsync(process.argv);
   const options = program.opts();
 
   if (!options.map || !options.envfile) {
-    throw new Error('Missing required arguments: --map and --envfile');
+    throw new Error("Missing required arguments: --map and --envfile");
   }
 
   await run(options.map, options.envfile);
 }
+
+cliRunner().catch((error) => {
+  console.error("Error in CLI Runner:", error);
+  process.exit(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,8 @@ import { GetParameterCommand, SSM } from "@aws-sdk/client-ssm";
 const ssm = new SSM({});
 
 export async function run(mapPath: string, envFilePath: string) {
-  console.log(`Reading content from file: ${mapPath}`);
   const content = fs.readFileSync(mapPath, "utf-8");
-  console.log(content);
-
-  console.log("Parsing JSON");
   const paramMap: Record<string, string> = JSON.parse(content);
-  console.log(`Parsed JSON: ${JSON.stringify(paramMap)}`);
 
   const envContent: string[] = [];
 
@@ -22,17 +17,14 @@ export async function run(mapPath: string, envFilePath: string) {
         WithDecryption: true,
       });
 
-      console.log(`Fetching parameter ${ssmName}`);
       const { Parameter } = await ssm.send(command);
       const value = Parameter?.Value;
       if (value) {
         envContent.push(`${envVar}=${value}`);
-        console.log(`Append: ${envVar}=${value}`);
+        console.log(`${envVar}=${value}`);
       } else {
         console.error(`Warning: No value found for ${ssmName}`);
       }
-
-      console.log("Writing to `.env` file...");
     } catch (error) {
       console.error(`Error fetching parameter ${ssmName}: ${error}`);
       throw new Error(`ParameterNotFound: ${ssmName}`);
