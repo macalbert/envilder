a17fbd361248df730a330b913eeb6e9f3838af63
Marçal Albert <marcal.albert@m47labs.com>
2024-09-30 23:37:45 +0200

    fix: import file

--- src/cli/cliRunner.ts
+++ src/cli/cliRunner.ts
@@ -1,29 +1,23 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import { run } from "../index";
+import { Command } from 'commander';
+import { run } from '../index.js';
 
 export async function cliRunner() {
   const program = new Command();
 
   program
-    .name("envilder")
-    .description("A CLI tool to generate .env files from AWS SSM parameters")
-    .version("0.1.0")
-    .requiredOption(
-      "--map <path>",
-      "Path to the JSON file with environment variable mapping"
-    )
-    .requiredOption(
-      "--envfile <path>",
-      "Path to the .env file to be generated"
-    );
+    .name('envilder')
+    .description('A CLI tool to generate .env files from AWS SSM parameters')
+    .version('0.1.0')
+    .requiredOption('--map <path>', 'Path to the JSON file with environment variable mapping')
+    .requiredOption('--envfile <path>', 'Path to the .env file to be generated');
 
   await program.parseAsync(process.argv);
   const options = program.opts();
 
   if (!options.map || !options.envfile) {
-    throw new Error("Missing required arguments: --map and --envfile");
+    throw new Error('Missing required arguments: --map and --envfile');
   }
 
   await run(options.map, options.envfile);
