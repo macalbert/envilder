de31f56c18996ff12b96dbb3722c118af1f947d6
Marçal Albert Castellví <mac.albert@hotmail.es>
2024-10-06 23:41:40 +0200

    apply code review comments

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     include: ['tests/**/*.test.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'json'],
       reportsDirectory: './coverage',
       all: true,
       include: ['src/**/*.ts'],
