d71e4e73e245ffc8afd65dee7e0052543ee3eef7
Marçal Albert Castellví <mac.albert@hotmail.es>
2024-10-06 22:54:44 +0200

    chore: Add coverage-report CI

--- vitest.config.js
+++ vitest.config.js
@@ -1,17 +1,12 @@
+// vitest.config.js
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    globals: true,
-    environment: 'node',
-    include: ['tests/**/*.test.ts'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
       reportsDirectory: './coverage',
-      all: true,
-      include: ['src/**/*.ts'],
-      exclude: ['node_modules', 'test', 'coverage', 'dist'],
     },
   },
 });
